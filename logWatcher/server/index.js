1acda23ffbbb89d9bb4fc1f36ea44885ef743e7a
Tushar Singh <tusharsingh@lidolearning.com>
2021-08-01 22:06:42 +0530

    used stream for processing log file

--- logWatcher/server/index.js
+++ logWatcher/server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const Tail = require('better-tail')
+const fs = require('fs');
+// const https = require('https');
 const app = express();
 const port = 4000;
 const cors = require('cors');
@@ -7,20 +8,45 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 const LINES = 10;
-const logs = [];
 
-app.get('/', (req, res) => res.json({logs}));
+const URL = '../logs.txt';
 
-const tail = new Tail('../logs.txt', { follow: true, lines: LINES });
-    tail.on('line', function (line) {
-      currentLine = line;
-      logs.push({line: currentLine, created_at: Date.now()});
-      if(logs.length > LINES) {
-        logs.shift();
+app.get('/', (req, res) => {
+  const stream = fs.createReadStream(URL);
+
+  // stream.resume();
+  let cnt=0;
+  let logs = [];
+
+  stream.on('open', function () {
+    console.log('stream opened');
+  });
+
+  stream.on('data', function (chunk) {
+    cnt++;
+    console.log('processing chunk', cnt);
+    const chunkOfLogs = chunk.toString().split(/\r\n|\n\r|\n|\r/);
+    const chunkSize = chunkOfLogs.length;
+    logs = chunkOfLogs.slice(Math.max(chunkSize - LINES, 0));
+  });
+
+  stream.on('error', function(err) {
+    console.log('Error reading the stream', err);
+    res.end(err);
+  });
+
+  stream.on('end', (data) => {
+    console.log('ending the read',  cnt, logs);
+    const response = logs.map(curr => {
+      return {
+        line: curr,
+        server_timestamp: Date.now()
       }
-  }).on('error', function (err) {
-      console.error(err)
-    })
+    });
+    // stream.pause();
+    res.status(200).send({logs:response});
+  });
+});
 
 app.listen(port, () => {
   console.log(`App server listening on port ${port}!`);
